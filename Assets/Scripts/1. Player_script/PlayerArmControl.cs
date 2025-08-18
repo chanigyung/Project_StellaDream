fac4db4e3859d035f2141fb68d5cd3d87a6e3e67
chanigyung <132416672+chanigyung@users.noreply.github.com>
2025-08-18 16:00:40 +0900

    Revert "추가 : 플레이어 애니메이션 추가"
    This reverts commit 8eec88700bdef9d983f3ff038ca0b9e776982ce5.

--- Assets/Scripts/1. Player_script/PlayerArmControl.cs
+++ Assets/Scripts/1. Player_script/PlayerArmControl.cs
@@ -67,6 +67,7 @@ public class PlayerArmControl : MonoBehaviour
         }
         else
         {
+            rightArm.rotation = Quaternion.Euler(0, 0, rightAngle);
             // Quaternion rightRot = Quaternion.Slerp(Quaternion.identity, leftArm.rotation, 0.5f);
             // rightArm.rotation = rightRot;
         }
